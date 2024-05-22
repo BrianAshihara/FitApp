ea29ca8e51dae5f2424a6b9faeb78ec085ed9518
Brian <brian09.c.a@gmail.com>
2024-05-21 23:03:04 -0300

    usuario check

--- database/migrations/2024_05_22_015802_usuario.php
+++ database/migrations/2024_05_22_015802_usuario.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class usuario extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -21,8 +21,8 @@ class usuario extends Migration
         });
     }
 
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('usuario');
     }
-}
+};
