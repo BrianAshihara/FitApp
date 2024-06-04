d4c7af178b8635b8ee7f65c4e6292f2da8d73670
Brian <brian09.c.a@gmail.com>
2024-06-04 20:55:58 -0300

    04/06.2

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/dashboard', [Dashboard::class,'dashboard'])->name('dashboard');
 
 //Chamando rota para usar o controller.
