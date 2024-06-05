5a9d8723f9a3a9d135fe411c371a2a041953f530
Caua Ernani <caualima1010@gmail.com>
2024-06-04 21:28:36 -0300

    teste de rota

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/dashboard', [Dashboard::class,'dashboard'])->name('dashboard');
+Route::get('/', function (){
+    return view('dashboard');
+});
+
+Route::get('/teste', function (){
+    return view('Vsf_Brian');
+});
 
 //Chamando rota para usar o controller.
 //Listar geral
