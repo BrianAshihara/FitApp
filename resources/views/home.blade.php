adbbb5c5728f0d6bc4fe0de6eec082a069826769
Brian <brian09.c.a@gmail.com>
2024-07-29 12:34:46 -0300

    home improvements

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -120,14 +120,13 @@
                   <div class="col-lg-6 col-md-7 offset-md-6 offset-md-5">
                     <div class="detail-box">
                       <h2>
-                        Get Your Body
+                        Construa seu SHAPE
                       </h2>
                       <h1>
-                        Fitness Here
+                        Fitness Aqui
                       </h1>
                       <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Ut enim ad minim veniam
+                        CONSTRUA O CORPO QUE VOCE SEMPRE SONHOU COM A FITAPP
                       </p>
                       <div class="btn-box">
                         <a href="" class="btn-1">
@@ -353,7 +352,7 @@
     <div class="container">
       <div class="heading_container">
         <h2>
-          Why Choose Us
+          Por que escolher FitApp?
         </h2>
       </div>
       <div class="us_container">
@@ -363,10 +362,10 @@
           </div>
           <div class="detail-box">
             <h5>
-              QUALITY EQUIPMENT
+              ACOMPANHAMENTO DE QUALIDADE
             </h5>
             <p>
-              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              Sinta-se confortável com o acompanhamento de qualidade que oferecemos ao longo do processo
             </p>
           </div>
         </div>
@@ -376,10 +375,10 @@
           </div>
           <div class="detail-box">
             <h5>
-              HEALTHY NUTRITION PLAN
+              PLANEJAMENTO DE SAÚDE ADEQUADO
             </h5>
             <p>
-              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                Toda a sua saúde é planejada de acordo com suas necessidades
             </p>
           </div>
         </div>
@@ -389,10 +388,10 @@
           </div>
           <div class="detail-box">
             <h5>
-              SHOWER SERVICE
+              SERVIÇO PERSONALIZADO
             </h5>
             <p>
-              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              Personalizamos nossos serviços de acordo com suas necessidades
             </p>
           </div>
         </div>
@@ -402,10 +401,10 @@
           </div>
           <div class="detail-box">
             <h5>
-              UNIQUE TO YOUR NEEDS
+              COMPARTILHE SEUS RESULTADOS
             </h5>
             <p>
-              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              Compartilhe com seus amigos e familiares os resultados obtidos!
             </p>
           </div>
         </div>
@@ -414,79 +413,6 @@
   </section>
 
   <!-- end us section -->
-
-
-  <!-- client section -->
-
-  <section class="client_section layout_padding">
-    <div class="container">
-      <div class="heading_container">
-        <h2>
-          What Says Our Customers
-        </h2>
-      </div>
-      <div id="carouselExample2Indicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          <li data-target="#carouselExample2Indicators" data-slide-to="0" class="active"></li>
-          <li data-target="#carouselExample2Indicators" data-slide-to="1"></li>
-          <li data-target="#carouselExample2Indicators" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <div class="box">
-              <div class="img-box">
-                <img src="{{ url ('assets/images/client.png')}}" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  Consectetur
-                </h5>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <div class="box">
-              <div class="img-box">
-                <img src="{{ url ('assets/images/client.png')}}" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  Consectetur
-                </h5>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <div class="box">
-              <div class="img-box">
-                <img src="{{ url ('assets/images/client.png')}}" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  Consectetur
-                </h5>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-    </div>
-  </section>
-
-  <!-- end client section -->
 
   <!-- result section -->
 
@@ -517,60 +443,6 @@
   </section>
 
   <!-- end result section -->
-
-
-  <!-- contact section -->
-  <section class="contact_section layout_padding">
-    <div class="container">
-      <div class="heading_container">
-        <h2>
-          <span>
-            Get In Touch
-          </span>
-        </h2>
-      </div>
-      <div class="layout_padding2-top">
-        <div class="row">
-          <div class="col-md-6 ">
-            <form action="">
-              <div class="contact_form-container">
-                <div>
-                  <div>
-                    <input type="text" placeholder="Name" />
-                  </div>
-                  <div>
-                    <input type="email" placeholder="Email" />
-                  </div>
-                  <div>
-                    <input type="text" placeholder="Phone Number" />
-                  </div>
-                  <div class="mt-5">
-                    <input type="text" placeholder="Message" />
-                  </div>
-                  <div class="mt-5">
-                    <button type="submit">
-                      Send
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </form>
-          </div>
-          <div class="col-md-6">
-            <div class="map_container">
-              <div class="map-responsive">
-                <iframe
-                  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&q=Eiffel+Tower+Paris+France"
-                  width="600" height="300" frameborder="0" style="border:0; width: 100%; height:100%"
-                  allowfullscreen></iframe>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  <!-- end contact section -->
 
 
   <!-- info section -->
@@ -671,7 +543,7 @@
             </a>
             <a href="">
               <img src="{{ url ('assets/images/mail-white.png')}}" alt="">
-              <span> demo123@gmail.com</span>
+              <span> fitappcontact@gmail.com</span>
             </a>
           </div>
           <div class="info_social">
